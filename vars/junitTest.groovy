f3d6dacb3d34cecbfbf65ca8723a340870ca660f
aymen <aymen.jaaouane@rte-france.com>
2017-10-11 21:58:06 +0200

    junit Module

---
+++ vars/junitTest.groovy
@@ -0,0 +1,20 @@
+def call(body) {
+	// evaluate the body block, and collect configuration into the object
+	def config = [:]
+	body.resolveStrategy = Closure.DELEGATE_FIRST
+	body.delegate = config
+	body()
+
+	def pathList = config.path ?: ['./'];
+	def ignoreFailure = config.ignoreFailure ?: true;
+
+	for(int i = 0; i < pathList.size(); i++){
+		def targetPath = pathList[i]
+
+		withMaven(maven: 'maven3'){  // Run the maven build
+			sh "cd ./${targetPath} && mvn test -Dmaven.test.failure.ignore=${ignoreFailure}"  }
+		// Recup des résultats de TU
+		junit '**/target/surefire-reports/TEST-*.xml'
+
+	}
+}
