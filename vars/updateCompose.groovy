6ba4f72848ca6cd6d1639a684b0f907840d7543c
aymen <aymen.jaaouane@rte-france.com>
2017-12-01 23:52:00 +0100

    updatecompose

---
+++ vars/updateCompose.groovy
@@ -0,0 +1,18 @@
+def call(body) {
+	// evaluate the body block, and collect configuration into the object
+	def config = [:]
+	body.resolveStrategy = Closure.DELEGATE_FIRST
+	body.delegate = config
+	body()
+
+        def projectName = config.projectName ?: 'ms-sample';
+        def pathList = config.path ?: ['./'];
+        def imgVersion = config.imgVersion ?: 'latest';
+
+
+        for(int i = 0; i < pathList.size(); i++){
+		sh "sed -i 's;${projectName}/${targetPath}:.*;${projectName}/${targetPath}:${imgVersion};g' docker-compose.yml";
+        }
+
+	
+}
