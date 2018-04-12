63312945839e99a89f44fc4847958bb486f9d98b
aymen <aymen.jaaouane@rte-france.com>
2018-04-12 12:00:21 +0200

    dockerbuild.groovy

---
+++ vars/deploy.groovy
@@ -0,0 +1,17 @@
+def call(body) {
+	// evaluate the body block, and collect configuration into the object
+	def config = [:]
+	body.resolveStrategy = Closure.DELEGATE_FIRST
+	body.delegate = config
+	body()
+
+        def inventoryFile = config.inventoryFile ;
+        def playbookFile = config.playbookFile ;
+        def extraVars = config.extraVars ;
+	
+        wrap([$class: 'AnsiColorBuildWrapper', colorMapName: "xterm"]) {        
+              ansiblePlaybook installation: 'ansible', inventory: inventoryFile, playbook: playbookFile, extraVars: extraVars
+        }
+
+	
+}
